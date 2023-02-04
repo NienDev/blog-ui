cafe1f6c72ceccfe6a57cc453f53c49cb61eb244
NienDev <niewebdev@gmail.com>
2023-02-04 21:46:37 +0700

    fixxx

--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -1,3 +1,4 @@
+import { Post } from "@/types";
 import { NextApiRequest, NextApiResponse } from "next";
 import data from "../data";
 export default async function handler(
@@ -6,13 +7,19 @@ export default async function handler(
 ) {
   const { postId } = req.query;
   const { Posts } = data;
+  if (!postId) {
+    res.status(404).json({ msg: "Data not found" });
+    return;
+  }
   let post = Posts.find((post) => post.id == Number(postId));
   if (post) {
     res.status(200).json(post);
   } else {
-    post = Posts.filter((post) => post.category.toLowerCase().includes(postId));
-    if (post) {
-      res.status(200).json(post);
+    let posts = Posts.filter((post) =>
+      post.category.toLowerCase().includes(postId[0])
+    );
+    if (posts) {
+      res.status(200).json(posts);
     } else res.status(404).json({ message: "data not found" });
   }
 }
