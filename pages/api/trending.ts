192b7f73901b392724a09adbe49ef9ecc060e44a
NienDev <niewebdev@gmail.com>
2023-02-05 14:08:56 +0700

    fixxxxxx

--- pages/api/trending.ts
+++ pages/api/trending.ts
@@ -7,6 +7,5 @@ export default async function handler(
   const { Trending } = data;
   if (Trending) {
     res.status(200).json({ Trending });
-  }
-  res.status(404).json({ message: "Data not found" });
+  } else res.status(404).json({ message: "Data not found" });
 }
